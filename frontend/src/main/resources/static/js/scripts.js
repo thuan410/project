642a732534a50b0497e5480a86ef625c47fb8aa1
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-23 17:59:28 +0700

    Thuan commit: second

--- frontend/src/main/resources/static/js/scripts.js
+++ frontend/src/main/resources/static/js/scripts.js
@@ -1,18 +1,14 @@
 function FetchUser() {
-    // Get the username entered by the user
+    // Lấy tên người dùng đã nhập
     const username = document.getElementById("username").value;
 
-    // Fetch user data based on the username
+    // Lấy dữ liệu người dùng dựa trên tên người dùng
     fetch(`http://54.172.124.101:8080/api/user/getByName?name=${username}`)
-        .then(response => response.json()).catch(error => alert("No users matched that name!!"))
+        .then(response => response.json())
+        .catch(error => alert("Không tìm thấy người dùng với tên này!"))
         .then(json => {
             console.log(json);
-            // Update the HTML with the fetched data
-            document.getElementById("userUsername").innerText = json.username;
-            document.getElementById("userEmail").innerText = json.email;
-            document.getElementById("userPassword").innerText = json.password;
-
+            // Bạn có thể thực hiện các hành động với dữ liệu json tại đây
         })
-        .catch(error => alert('Error fetching data:', error));
+        .catch(error => alert('Lỗi khi lấy dữ liệu:', error));
 }
-//test2
