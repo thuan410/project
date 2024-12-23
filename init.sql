bf57b24df62d36e0b0003dc92e160de2594b6b3e
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-23 21:16:10 +0700

    Thuan commit: 7

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE nhom1table (
+CREATE TABLE table (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100),
     email VARCHAR(100),
@@ -6,6 +6,6 @@ CREATE TABLE nhom1table (
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO nhom1table (username, email, password) VALUES
+INSERT INTO table (username, email, password) VALUES
 ('thuan', tvt04102004@gmail.com, 410),
 ('thuan1', tranvanthuan04102004@gmail.com, 410);
